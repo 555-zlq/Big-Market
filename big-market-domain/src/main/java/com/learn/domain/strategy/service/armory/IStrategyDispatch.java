1474b8eeac0ec7b8cc54e0c9356067ad30c8eac8
carton <2181058263@qq.com>
2025-10-09 23:46:36 +0800

    feat: 策略权重概率装配

---
+++ big-market-domain/src/main/java/com/learn/domain/strategy/service/armory/IStrategyDispatch.java
@@ -0,0 +1,29 @@
+package com.learn.domain.strategy.service.armory;
+
+
+/**
+ * @author Carton
+ * @date 2025/10/9 10:38
+ * @description TODO: 策略抽奖的调度接口
+ */
+
+public interface IStrategyDispatch {
+
+    /**
+     * 根据策略id获取随机奖品
+     *
+     * @param strategyId
+     *
+     */
+    Integer getRandomAwardId(Long strategyId);
+
+    /**
+     * 根据策略id以及规则权重值获取随机奖品
+     *
+     * @param strategyId
+     * @param ruleWeightValue
+     *
+     */
+    Integer getRandomAwardId(Long strategyId, String ruleWeightValue);
+
+}
